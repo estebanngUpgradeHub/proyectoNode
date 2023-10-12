87192a26b3cd69b460dd4e6f5a2660b167dda90c
jesus1490 <jesusperez_14@hotmail.com>
2023-10-12 12:57:51 +0200

    PUT y DELETE

--- src/api/controllers/juegos.controller.js
+++ src/api/controllers/juegos.controller.js
@@ -11,6 +11,7 @@ const newGame = async (req, res) => {
     }
 };
 
+//Get
 const getGames = async (req, res) => {
     try {
         const allGames = await Juegos.find();
@@ -20,7 +21,36 @@ const getGames = async (req, res) => {
 }
 }
 
+//Put
+const updateGame = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const gameBody = new Juegos(req.body);
+        gameBody._id = id;
+        const updateGame = await Juegos.findByIdAndUpdate(id, gameBody, { new: true });
+    if (!updateGame) {
+        return res.status(404).json({ message: "Game does not exist" })
+    }
+    return res.status(200).json(updateGame)
+} catch (error) {
+
+}
+}
+
+//Delete
+const deleteGame = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteGame = await Movies.findByIdAndDelete(id);
+        if (!deleteGame) {
+            return res.status(404).json({ message: "Game does not exist" })
+        }
+        return res.status(200).json(deleteGame)
+
+    } catch (error) {
+
+    }
+}
 
 
-
-module.exports = { newGame, getGames };
+module.exports = { newGame, getGames, updateGame, deleteGame };
--- src/api/routes/juegos.routes.js
+++ src/api/routes/juegos.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
-const {newGame, getGames} = require('../controllers/juegos.controller'); 
+const {newGame, getGames, updateGame, deleteGame} = require('../controllers/juegos.controller'); 
 
 const router = express.Router();
 
 router.post('/register', newGame);
+router.delete('/juegos', deleteGame);
+router.put('/juegos', updateGame);
 router.get('/', getGames);
 
 
