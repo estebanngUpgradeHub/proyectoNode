ded535a3f6d956cbc4e38c4f1aab43af413e39c7
jesus1490 <jesusperez_14@hotmail.com>
2023-10-12 11:20:58 +0200

    Post newGame

--- src/api/controllers/juegos.controller.js
+++ src/api/controllers/juegos.controller.js
@@ -1,1 +1,16 @@
-const Juegos = require('../models/juegos.controller');
+const Juegos = require('../models/juegos.model');
+
+//Post
+const newGame = async (req, res) => {
+    try {
+        const game = new Juegos(req.body);
+        const createdGame = await game.save();
+        return res.status(201).json(createdGame);
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+};
+
+
+
+module.exports = { newGame };
