7fb84acdaf0b09bea87e3e6b34435edcb352fbb6
jesus1490 <jesusperez_14@hotmail.com>
2023-10-12 19:19:27 +0200

    user routes

---
+++ src/api/routes/user.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const { registerUser, loginUser, profileUser } = require('../controllers/user.controller');
+// const { isAuth } = require('../')
+const router = express.Router();
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/profile', [isAuth], profileUser);
+
+module.exports = router;
