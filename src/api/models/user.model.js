b2562cb4e12b63ce7f07225a0c7dd56725a9006a
paulagimeno <paula-gl-98@hotmail.com>
2023-10-12 19:11:45 +0200

    user models

---
+++ src/api/models/user.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose'); 
+const Schema = mongoose.Schema; 
+
+const userSchema = new Schema({
+    email:{type: String, required: true}, 
+    username: {type: String, required: true}, 
+    password: {type: String, required: true}, 
+    role: {type: String, default: "user", enum: ["admin", "user"]},
+    avatar: {type: String},
+    gender: {type: String, enum: ["Male", "Female"], required: true}
+},{
+    collection: "user"
+});
+
+const User = mongoose.model("user", userSchema);
+module.exports = User; 
