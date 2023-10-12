e80db6a74dbeb7d6ac9e292229069af104d819e1
jesus1490 <jesusperez_14@hotmail.com>
2023-10-12 10:54:13 +0200

    juegos model

---
+++ src/api/models/juegos.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+//Para utilizar mongoose//
+const juegoSchema = new Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    genre: {type: String, required: true},
+    playerNumber : {type: Number, required: true},
+    price: {type: Number, required: true},
+    developer: {type: String, required: true},
+    realeaseDate: {type: Date, required: true},
+},{
+collection: 'juego'
+});
+//en las primeras llaves se situan los datos que podemos introducir cuando hacemos post, si añadimos el atributo required será obligatorio.//
+//posibles tipos de datos: string, number, array, date, mixed, boolean, objectId//
+const Juego = mongoose.model('juego', juegoSchema)
+module.exports = Juego;
