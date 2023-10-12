1ac03e4b6d8b2f648166a3407119c9cbb2e60a71
paulagimeno <paula-gl-98@hotmail.com>
2023-10-12 20:02:00 +0200

    muchas cosas

--- src/api/routes/user.routes.js
+++ src/api/routes/user.routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 const { registerUser, loginUser, profileUser } = require('../controllers/user.controller');
-// const { isAuth } = require('../')
+const { isAuth } = require('../../middleware/auth')
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-// router.get('/profile', [isAuth], profileUser);
+router.get('/profile', [isAuth], profileUser);
 
 module.exports = router;
