b709c3c9a30407b22460d5a0f85efa30dc1cf479
jesus1490 <jesusperez_14@hotmail.com>
2023-10-12 18:50:50 +0200

    users index

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const {connectDb} = require('./src/utils/database');
 const routeJuegos = require('./src/api/routes/juegos.routes')
+const routesUser = require('./src/api/routes/user.routes')
+
 
 const app = express();
 connectDb()
 app.use(express.json());
-app.use('/juegos', routeJuegos)
+app.use('/juegos', routeJuegos);
+app.use('/users', routeUsers);
 
 const PORT = 5051;
 app.listen(PORT, () => {
