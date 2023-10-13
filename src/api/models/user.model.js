7c9c1676bede4b46052c9b3afd672d69213fd8a4
jesus1490 <jesusperez_14@hotmail.com>
2023-10-13 16:22:38 +0200

    fuck avatar

--- src/api/models/user.model.js
+++ src/api/models/user.model.js
@@ -7,14 +7,6 @@ const userSchema = new Schema({
     username: {type: String, required: true}, 
     password: {type: String, required: true}, 
     role: {type: String, default: "user", enum: ["admin", "user"]},
-    avatar: {type: String, default:  avatarP = () => {
-        if (User.gender === "Female" ) {
-            return "https://res.cloudinary.com/dizd9f3ky/image/upload/v1697206175/imageFemale_efggh9.png"
-        } else if (User.gender === "Male" ) {
-            return "https://res.cloudinary.com/dizd9f3ky/image/upload/v1697206175/imageMale_mqwzhf.png"
-        }
-    
-    },},
     gender: {type: String, enum: ["Male", "Female"], required: true}
 },{
     
