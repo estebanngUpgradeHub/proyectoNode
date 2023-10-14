35cfcca7aa4fb72b38d9d114b8bb05719166a1b0
jesus1490 <jesusperez_14@hotmail.com>
2023-10-14 20:01:11 +0200

    Login CSS

---
+++ FRONT/Prueba log in/LogIn/login.js
@@ -0,0 +1,17 @@
+document.getElementById('loginButton').addEventListener('click', function (event) {
+    event.preventDefault(); // Evita que el formulario se envíe
+
+    // Obtén los valores de correo electrónico y contraseña
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    // Aquí puedes realizar la validación de correo electrónico y contraseña
+    if (email === 'tu@email.com' && password === 'tupassword') {
+        // Si las credenciales son válidas, redirige al usuario a una página de inicio, por ejemplo:
+        window.location.href = 'pagina-de-inicio.html';
+    } else {
+        // Si las credenciales no son válidas, muestra un mensaje de error
+        alert('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
+    }
+});
+
