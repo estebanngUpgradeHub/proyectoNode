78dbc21f1d36e6f185fe2f4324da14a1bc72eb47
paulagimeno <paula-gl-98@hotmail.com>
2023-10-14 22:44:50 +0200

    fetch

--- FRONT/home.js
+++ FRONT/home.js
@@ -1,7 +1,26 @@
-fetch('http://localhost:5051/juegos')
-  .then((response) => {
-    return response.json();
-  })
-  .then((myJson) => {
-    console.log(myJson);
-  });
+// fetch('http://localhost:5051/juegos')
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((games) => {
+//     console.log(games);
+//   });
+
+//   const $carusel = document.querySelector('.foto_random')
+
+//   for (let i = 0; i < games; i++) {
+//     const game = games[i];
+//     $carusel.setAttribute('src', game.image)
+    
+//   }
+
+async function getGames(){
+  let allGames = [];
+  for (let i = 0; i < 30; i++) {
+    const response = await fetch ('http://localhost:5051/juegos')
+    const resultado = await response.json()
+    allGames.push(resultado);
+  }
+  console.log(allGames)
+  return allGames;
+}
