e4526f4b161946c5621d0ac0f5646d193f3730c4
paulagimeno <paula-gl-98@hotmail.com>
2023-10-14 20:40:48 +0200

    ya funciona

--- FRONT/Prueba log in/LogIn/login.js
+++ FRONT/Prueba log in/LogIn/login.js
@@ -1,17 +1,20 @@
-document.getElementById('loginButton').addEventListener('click', function (event) {
-    event.preventDefault(); // Evita que el formulario se envíe
+// document.getElementById('loginButton').addEventListener('click', function (event) {
+//     event.preventDefault(); // Evita que el formulario se envíe
 
-    // Obtén los valores de correo electrónico y contraseña
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
+//     // Obtén los valores de correo electrónico y contraseña
+//     var email = document.getElementById('email').value;
+//     var password = document.getElementById('password').value;
 
-    // Aquí puedes realizar la validación de correo electrónico y contraseña
-    if (email === 'tu@email.com' && password === 'tupassword') {
-        // Si las credenciales son válidas, redirige al usuario a una página de inicio, por ejemplo:
-        window.location.href = 'pagina-de-inicio.html';
-    } else {
-        // Si las credenciales no son válidas, muestra un mensaje de error
-        alert('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
-    }
-});
+//     // Aquí puedes realizar la validación de correo electrónico y contraseña
+//     if (email === 'tu@email.com' && password === 'tupassword') {
+//         // Si las credenciales son válidas, redirige al usuario a una página de inicio, por ejemplo:
+//         window.location.href = 'pagina-de-inicio.html';
+//     } else {
+//         // Si las credenciales no son válidas, muestra un mensaje de error
+//         alert('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
+//     }
+// });
+
+
+
 
